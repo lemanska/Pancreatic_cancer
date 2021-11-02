bb3f5026bc0ab9c70b79490f9c082181f5e6f4f2
lemanska <a.lemanska@surrey.ac.uk>
2021-11-02 21:05:02 +0000

    Generating study population and variables

---
+++ analysis/codelists.py
@@ -0,0 +1,44 @@
+from cohortextractor import (
+    codelist_from_csv,
+    codelist,
+)
+
+# DEMOGRAPHIC CODELIST
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
+ethnicity_codes_16 = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_16",
+)
+# cancer
+
+cancer_referral_codes = codelist_from_csv(
+    "codelists/user-agleman-pa_ca_referrals.csv",
+    system="ctv3",
+    column="code",
+)
+
+chemotherapy_or_radiotherapy_codes = codelist_from_csv(
+    "codelists/opensafely-chemotherapy-or-radiotherapy.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+pan_cancer_codes = codelist_from_csv(
+    "codelists/Pancreatic_cancer_ctv3.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+# HbA1c
+hba1c_new_codes = codelist(["XaPbt", "Xaeze", "Xaezd"], system="ctv3")
+
+# Ca death 
+pa_ca_icd10 = codelist(["C25", "C25.0", "C25.1", "C25.2", "C25.3", "C25.4", 
+                     "C25.7", "C25.8", "C25.8"], system ="icd10")
