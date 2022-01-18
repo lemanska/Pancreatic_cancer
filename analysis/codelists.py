0066881a35800b11efecaddc0cbe9c530145b03a
lemanska <a.lemanska@surrey.ac.uk>
2022-01-18 15:45:14 +0000

    test
    test

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -16,10 +16,35 @@ ethnicity_codes_16 = codelist_from_csv(
     column="Code",
     category_column="Grouping_16",
 )
-# cancer
+# Symptoms
 
+jaundice = codelist_from_csv(
+    "codelists/user-agleman-jaundice.csv",
+    system="snomed",
+    column="code",
+)
+
+# tests
+ca19_9 = codelist_from_csv(
+    "codelists/user-agleman-ca_19_9.csv",
+    system="snomed",
+    column="code",
+)
+CEA = codelist_from_csv(
+    "codelists/user-agleman-cea.csv",
+    system="snomed",
+    column="code",
+)
+# Fast track referrals for paca
 cancer_referral_codes = codelist_from_csv(
     "codelists/user-agleman-cancer_referrals_pancreatic_cancer_snomed.csv",
+    system="snomed",
+    column="code",
+)
+# treatment - enzyme replacement 
+# this is not right as it is a medication - should be dmd system?
+enzyme_replace = codelist_from_csv(
+    "codelists/user-agleman-pancreatic-enzyme-replacement-therapy.csv",
     system="snomed",
     column="code",
 )
@@ -35,10 +60,26 @@ pan_cancer_codes = codelist_from_csv(
     system="snomed",
     column="code",
 )
+diabetes_codes = codelist_from_csv(
+    "codelists/opensafely-diabetes.csv", system="ctv3", column="CTV3ID"
+)
+
+liver_funct_codes = codelist_from_csv(
+    "codelists/user-agleman-liver_function_tests.csv",
+    system="snomed",
+    column="code",
+)
+
+# pa ca icd10 for admisions and death
+# pa_ca_icd10 = codelist(["C25", "C25.0", "C25.1", "C25.2", 
+# "C25.3", "C25.4",
+# "C25.7", "C25.8", "C25.8"], system ="icd10")
+
+pa_ca_icd10 = codelist_from_csv(
+    "codelists/user-agleman-pancreatic-cancer-icd10.csv",
+    system="icd10",
+    column="code",
+)
 
 # HbA1c
 hba1c_new_codes = codelist(["XaPbt", "Xaeze", "Xaezd"], system="ctv3")
-
-# Ca death 
-pa_ca_icd10 = codelist(["C25", "C25.0", "C25.1", "C25.2", "C25.3", "C25.4", 
-                     "C25.7", "C25.8", "C25.8"], system ="icd10")
--- analysis/study_definition_main.py
+++ analysis/study_definition_main.py
@@ -1,38 +1,32 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv
-
+from cohortextractor import (
+    StudyDefinition,
+    patients
+)
 from codelists import *
 
 window_date = "2015-01-01"
 
+# new line 
+
 study = StudyDefinition(
-        default_expectations={
+    default_expectations={
         "date": {"earliest": "1900-01-01", "latest": "today"},
         "rate": "uniform",
         "incidence": 0.5,
         },
-    
-    #index_date = "", will be cancer diagnosis for each pt
-    
+    # index_date = "", it will be cancer diagnosis for each pt
     population=patients.satisfying(
-        """
-        pa_ca AND
-        (age >=18 AND age <= 110)
-        """
-        ),
+        """pa_ca AND
+        (age >=18 AND age <= 110)"""
+    ),
     pa_ca=patients.with_these_clinical_events(
         pan_cancer_codes,
-        on_or_after="1950-01-01", #no restriction or time, or sometime in 1950, looking for date of first diagnosis 
-        find_last_match_in_period=True,
-        # include_date_of_match=True,
-        # include_month=True, 
-        # include_day=True,
-        returning="binary_flag", # later could do it as cat for the type of pa ca
+        returning="binary_flag",
         return_expectations={"incidence": 1.0},
     ),
     ca_date=patients.with_these_clinical_events(
         pan_cancer_codes,
-        on_or_after="1950-01-01",
-        find_first_match_in_period=True, # find out when it was first diagnosed
+        find_first_match_in_period=True,
         returning="date",
         date_format="YYYY-MM-DD",
         return_expectations={"incidence": 1.0},
@@ -54,21 +48,20 @@ study = StudyDefinition(
         ethnicity_codes,
         returning="category",
         find_last_match_in_period=True,
-        include_date_of_match=True,
-        include_month=True, 
-        include_day=True,
         return_expectations={
             "category": {"ratios": {"1": 0.8, "2": 0.01, "3": 0.01, "4": 0.18}},
             "incidence": 0.75,
-        },
+        }
     ),
+    # Regional variation
     stp=patients.registered_practice_as_of(
         "ca_date",
         returning="stp_code",
         return_expectations={
             "rate": "universal",
-            "category": {"ratios": {"E54000005": 0.25, "E54000006": 0.25,
-            "E54000007": 0.25, "E54000008": 0.25}},
+            "category": {"ratios": {
+                "E54000005": 0.25, "E54000006": 0.25,
+                "E54000007": 0.25, "E54000008": 0.25}},
         },
     ),
     region=patients.registered_practice_as_of(
@@ -96,23 +89,37 @@ study = StudyDefinition(
         returning="index_of_multiple_deprivation",
         round_to_nearest=100,
         return_expectations={
-        "rate": "universal",
-        "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
+            "rate": "universal",
+            "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
         },
     ),
-    imd_quin=patients.categorised_as(
-        {   "No data": "DEFAULT", #this is missing data? 
-            "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
-            "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
-            "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
-            "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
-            "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation < 32844""",
-        },#took it from ?, is this the right way to calc quintiles? 
+    imd_quin=patients.categorised_as({
+        "No data": "DEFAULT",
+        "1": """
+        index_of_multiple_deprivation >=1
+        AND index_of_multiple_deprivation < 32844*1/5
+        """,
+        "2": """
+        index_of_multiple_deprivation >= 32844*1/5
+        AND index_of_multiple_deprivation < 32844*2/5
+        """,
+        "3": """
+        index_of_multiple_deprivation >= 32844*2/5
+        AND index_of_multiple_deprivation < 32844*3/5
+        """,
+        "4": """
+        index_of_multiple_deprivation >= 32844*3/5
+        AND index_of_multiple_deprivation < 32844*4/5""",
+        "5": """
+        index_of_multiple_deprivation >= 32844*4/5
+        AND index_of_multiple_deprivation < 32844""",
+        },
+        # took it from ?, is this the right way to calc quintiles?
         index_of_multiple_deprivation=patients.address_as_of(
             "ca_date",
             returning="index_of_multiple_deprivation",
             round_to_nearest=100,
-    ),
+        ),
         return_expectations={
             "rate": "universal",
             "category": {
@@ -127,37 +134,38 @@ study = StudyDefinition(
             },
         },
     ),
-    # BMI
+    # BMI, this is a most recent BMI 
+    #how can I get BMI that is closet to the event? 
     bmi=patients.most_recent_bmi(
         between=["ca_date - 1 years", "ca_date + 1 years"],
-        minimum_age_at_measurement=18,
+        minimum_age_at_measurement=16,
         include_measurement_date=True,
         date_format="YYYY-MM-DD",
         return_expectations={
-        "date": {"earliest": "2015-01-01", "latest": "today"},
-        "float": {"distribution": "normal", "mean": 25, "stddev": 8},
-        "incidence": 0.80
+            "date": {"earliest": "1900-01-01", "latest": "today"},
+            "float": {"distribution": "normal", "mean": 25, "stddev": 8},
+            "incidence": 0.80
         }
     ),
-    bmi_cat=patients.categorised_as(
-        {   "No data": "DEFAULT",  
-            "Underweight (<18.5)": """ bmi_value >= 5 AND bmi_value < 18.5""",
-            "Normal (18.5-24.9)": """ bmi_value >= 18.5 AND bmi_value < 25""",
-            "Overweight (25-29.9)": """ bmi_value >= 25 AND bmi_value < 30""",
-            "Obese I (30-34.9)": """ bmi_value >= 30 AND bmi_value < 35""",
-            "Obese II (35-39.9)": """ bmi_value >= 35 AND bmi_value < 40""",
-            "Obese III (40+)": """ bmi_value >= 40 AND bmi_value < 100"""
-        },
+    bmi_cat=patients.categorised_as({
+        "No data": "DEFAULT",  
+        "Underweight (<18.5)": """ bmi_value >= 5 AND bmi_value < 18.5""",
+        "Normal (18.5-24.9)": """ bmi_value >= 18.5 AND bmi_value < 25""",
+        "Overweight (25-29.9)": """ bmi_value >= 25 AND bmi_value < 30""",
+        "Obese I (30-34.9)": """ bmi_value >= 30 AND bmi_value < 35""",
+        "Obese II (35-39.9)": """ bmi_value >= 35 AND bmi_value < 40""",
+        "Obese III (40+)": """ bmi_value >= 40 AND bmi_value < 100"""
+    },
         bmi_value=patients.most_recent_bmi(
             between=["ca_date - 1 years", "ca_date + 1 years"],
             minimum_age_at_measurement=16
-            ),
+        ),
         return_expectations={
             "rate": "universal",
             "category": {
                 "ratios": {
                     "No data": 0.2,
-                    "Underweight (<18.5)": 0.2, #ppl lose a lot of weight when they are diagnosed with PaCa
+                    "Underweight (<18.5)": 0.2,
                     "Normal (18.5-24.9)": 0.3,
                     "Overweight (25-29.9)": 0.1, 
                     "Obese I (30-34.9)": 0.1,
@@ -170,44 +178,107 @@ study = StudyDefinition(
     latest_hba1c=patients.with_these_clinical_events(
         hba1c_new_codes,
         find_last_match_in_period=True,
+        # closest to the ca_date?
         between=["ca_date - 1 years", "ca_date + 1 years"],
-        returning="numeric_value", # how do I restrick on the possible range? To exclude impossible values? 
+        returning="numeric_value",
+        # how do I restrict on the possible range? To exclude impossible values
+        include_date_of_match=True,
+        include_month=True,
+        include_day=True,
+        return_expectations={
+            "float": {"distribution": "normal", "mean": 40.0, "stddev": 20},
+            "incidence": 0.95,
+        }
+        # this code gives me 0 for missing values - is there a way to have NAs? 
+    ),
+    diabetes=patients.with_these_clinical_events(
+        diabetes_codes,
+        returning="binary_flag",
+        # return_first_date_in_period=True,
         include_date_of_match=True,
         include_month=True, 
         include_day=True,
-        return_expectations={
-        "float": {"distribution": "normal", "mean": 40.0, "stddev": 20},
-        "incidence": 0.95,
-        }
-        #this code gives me 0 for missing values - is there a way to have NAs? 
+        return_expectations={"incidence": 0.30},
     ),
-    hba1c_date=patients.date_of("latest_hba1c", date_format="YYYY-MM-DD"),
-        #hba1c_comparator=patients.comparator_from("latest_hba1c"),
-        chemo_or_radio=patients.with_these_clinical_events(
-        chemotherapy_or_radiotherapy_codes,
-        on_or_before="today",
-        returning="binary_flag",
-        return_expectations={"incidence": 0.01,},
-    ),
-    #cancer variables 
-    Referrals=patients.with_these_clinical_events(
-        cancer_referral_codes,
-        #on_or_before="ca_date",
+    # Diagnostic tests
+    liver_funct=patients.with_these_clinical_events(
+        liver_funct_codes,
         between=["ca_date - 6 months", "ca_date"],
-        find_first_match_in_period=True, 
-        returning="date",
-        date_format="YYYY-MM-DD",
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        return_expectations={"incidence": 0.50},
+    ),
+    ca19_9=patients.with_these_clinical_events(
+        ca19_9,
+        between=["ca_date - 6 months", "ca_date"],
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        return_expectations={"incidence": 0.20},
+    ),
+    CEAntigen=patients.with_these_clinical_events(
+        CEA,
+        between=["ca_date - 6 months", "ca_date"],
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        return_expectations={"incidence": 0.10},
+    ),
+    # Symptoms, jaundice example 
+    jaundice=patients.with_these_clinical_events(
+        jaundice,
+        between=["ca_date - 6 months", "ca_date"],
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        return_expectations={"incidence": 0.60},
+    ),
+    # Referrals from primary care 
+    Refer=patients.with_these_clinical_events(
+        cancer_referral_codes,
+        # on_or_before="ca_date",
+        between=["ca_date - 6 months", "ca_date"],
+        find_last_match_in_period=True,
+        returning="binary_flag",
+        # returning="date", # can I have a count? 
+        # date_format="YYYY-MM-DD",
         return_expectations={"incidence": 0.6},
     ),
-
-    admitted=patients.admitted_to_hospital(
-        returning="binary_flag",
-        between=["ca_date - 6 months", "ca_date + 6 months"],
-        return_expectations={"incidence": 0.1}
-        # retrieve the number of admissions? 
+    # treatment in primary care - panc enzymes new prescriptions 
+    enz_repl=patients.with_these_clinical_events(
+        enzyme_replace,
+        # on_or_before="ca_date",
+        between=["ca_date - 6 months", "ca_date"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        }
     ),
-    ca_adm_date=patients.admitted_to_hospital(
-        returning= "date_admitted",
+    adm_before=patients.admitted_to_hospital(
+        between=["ca_date - 6 months", "ca_date"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 2, "stddev": 1},
+            "incidence": 0.4,
+        }
+    ),
+    adm_after=patients.admitted_to_hospital(
+        between=["ca_date", "ca_date + 6 months", ],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 3},
+            "incidence": 0.6,
+        }
+    ),
+    adm_ca=patients.admitted_to_hospital(
+        with_these_diagnoses=pa_ca_icd10,
+        between=["ca_date - 6 months", "ca_date + 6 months", ],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 2, "stddev": 2},
+            "incidence": 0.3,
+        }
+    ),
+    adm_ca_date=patients.admitted_to_hospital(
+        returning="date_admitted",
         with_these_diagnoses=pa_ca_icd10,
         on_or_after="ca_date",
         find_first_match_in_period=True,
@@ -216,24 +287,20 @@ study = StudyDefinition(
     ),
     emergency_care_before=patients.attended_emergency_care(
         between=["ca_date - 6 months", "ca_date"],
-        returning="date_arrived",
-        date_format="YYYY-MM-DD",
-        find_first_match_in_period=True,
+        returning="number_of_matches_in_period",
         return_expectations={
-        "date": {"earliest" : "2014-06-01"},
-        "rate" : "exponential_increase"
-        },
+            "int": {"distribution": "normal", "mean": 2, "stddev": 2},
+            "incidence": 0.3,
+        }
     ),
 
     emergency_care_after=patients.attended_emergency_care(
         between=["ca_date", "ca_date + 6 months"],
-        returning="date_arrived",
-        date_format="YYYY-MM-DD",
-        find_first_match_in_period=True,
+        returning="number_of_matches_in_period",
         return_expectations={
-        "date": {"earliest" : "2015-06-01"},
-        "rate" : "exponential_increase"
-        },
+            "int": {"distribution": "normal", "mean": 2, "stddev": 2},
+            "incidence": 0.3,
+        }
     ),
 
     died=patients.died_from_any_cause(
@@ -247,18 +314,16 @@ study = StudyDefinition(
         returning="date_of_death",
         date_format="YYYY-MM-DD",
         return_expectations={
-        "date": {"earliest" : "2015-01-01"},
-        "rate" : "exponential_increase",
-        "incidence": 0.80
+            "date": {"earliest": "2015-01-01"},
+            "rate": "exponential_increase",
+            "incidence": 0.80
         },
     ),
     died_ca=patients.with_these_codes_on_death_certificate(
         pa_ca_icd10,
         on_or_after="ca_date",
         match_only_underlying_cause=False,
-        return_expectations={
-        "incidence": 0.50
-        },
+        return_expectations={"incidence": 0.50},
     ),
     died_ca_date=patients.with_these_codes_on_death_certificate(
         pa_ca_icd10,
@@ -267,45 +332,44 @@ study = StudyDefinition(
         returning="date_of_death",
         date_format="YYYY-MM-DD",
         return_expectations={
-        "date": {"earliest" : "2015-01-01"},
-        "rate" : "exponential_increase",
-        "incidence": 0.50
+            "date": {"earliest": "2015-01-01"},
+            "rate": "exponential_increase",
+            "incidence": 0.50
         },
     ),
     gp_count=patients.with_gp_consultations(
         between=["ca_date - 1 years", "ca_date"],
         returning="number_of_matches_in_period",
         return_expectations={
-        "int": {"distribution": "normal", "mean": 6, "stddev": 3},
-        "incidence": 0.6,
+            "int": {"distribution": "normal", "mean": 6, "stddev": 3},
+            "incidence": 0.6,
         },
     ),
     care_home_type=patients.care_home_status_as_of(
         "ca_date",
         categorised_as={
-        "PC":
-        """
-          IsPotentialCareHome
-          AND LocationDoesNotRequireNursing='Y'
-          AND LocationRequiresNursing='N'
-        """,
-        "PN":
-        """
-          IsPotentialCareHome
-          AND LocationDoesNotRequireNursing='N'
-          AND LocationRequiresNursing='Y'
-        """,
-        "PS": "IsPotentialCareHome",
-        "PR": "NOT IsPotentialCareHome",
-        "": "DEFAULT",
+            "PC":
+            """
+            IsPotentialCareHome
+            AND LocationDoesNotRequireNursing='Y'
+            AND LocationRequiresNursing='N'
+            """,
+            "PN":
+            """
+            IsPotentialCareHome
+            AND LocationDoesNotRequireNursing='N'
+            AND LocationRequiresNursing='Y'
+            """,
+            "PS": "IsPotentialCareHome",
+            "PR": "NOT IsPotentialCareHome",
+            "": "DEFAULT",
         },
-    return_expectations={
-        "rate": "universal",
-        "category": {"ratios": {"PC": 0.05, "PN": 0.05, "PS": 0.05, "PR": 0.84, "": 0.01},},
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"PC": 0.05, "PN": 0.05, "PS": 0.05, "PR": 0.84, "": 0.01},},
         },
     ),
     # hh_size=patients.household_as_of(
     # "2020-02-01", returning="household_size"
     # )
 )
-###2/11/21
