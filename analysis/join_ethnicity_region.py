6ab5ad8bc23fd7d9df654abf1fef9767f1b5ccba
lemanska <a.lemanska@surrey.ac.uk>
2022-02-14 23:33:27 +0000

    Redoin study defin & calc measures

---
+++ analysis/join_ethnicity_region.py
@@ -0,0 +1,3 @@
+from utilities import join_ethnicity_region, OUTPUT_DIR
+
+join_ethnicity_region(OUTPUT_DIR)
--- analysis/report.R
+++ analysis/report.R
@@ -3,73 +3,91 @@ library(here)
 library(ggplot2)
 library(dplyr)
 
-fs::dir_create(here::here("output", "measures"))
 
-#test figure of age 
-df_input <- read.delim(here("output", "input_main.csv"), header=TRUE, sep=",")
-plot_age <- ggplot(data=df_input, aes(age)) + geom_histogram()
-ggsave(plot= plot_age,
-  filename="age_plot1.png", path=here::here("output"))
+########
+#pancreatic cancer rates 
+########
 
-#fig of paca diagnosis over time 
-df_input$ca_date <- as.Date(df_input$ca_date,format = "%Y-%m-%d") 
-df_input$Month <- as.Date(cut(df_input$ca_date, breaks = "month"))
+measure_paca <- read_csv(here::here("output","measures",
+                                    "measure_pa_ca_diagnosis_rate.csv"))
+measure_paca$date <- as.Date(measure_paca$date,format = "%Y-%m-%d") 
 
-paca_time <- ggplot(data = df_input,
-                    aes(Month, pa_ca)) +
-  stat_summary(fun = sum, geom = "line") + # or "bar"
+paca_rate <- ggplot(data = measure_paca,
+                    aes(date, value)) +
+  geom_line()+
+  geom_point()+
   scale_x_date(date_breaks = "2 month",
-               date_labels = "%Y-%m")+# or %B
+               date_labels = "%Y-%m")+#%B
   xlab("Time")+
-  ylab("Pa Ca diagn (count)")+
+  ylab("Pa Ca diagn (rate)")+
   labs(colour = "Station")+
   theme_bw()+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 ggsave(
-  plot= paca_time, dpi=800,width = 20,height = 10, units = "cm",
-  filename="paca_no.png", path=here::here("output"))
+  plot= paca_rate, dpi=800,width = 20,height = 10, units = "cm",
+  filename="paca_rate.png", path=here::here("output"),
+)
 
-#####generate a table of counts per month 
-paca_counts_pm <- table(df_input$Month)
-write.table(paca_counts_pm, here::here("output", "paca_counts_pm.csv"),
-          col.names= c("date","count"),sep = ",",row.names = F)
+write.table(measure_paca, here::here("output", "paca_rate.csv"),
+            col.names= c("count", "population", "rate", "date"),sep = ",",row.names = F)
 
-##########################################################################
-### this does not work ####
-##########################################################################
+########
+#pancreatic cancer rates ***by region***
+########
 
-#measure_paca <- read.delim(here("output", "measures",
-#                                "measure_pa_ca_diagnosis.csv"), 
-#                           header=TRUE, sep=",")
+measure_paca_region <- read_csv(here::here("output","measures",
+                                           "measure_pa_ca_by_region_rate.csv"))
+measure_paca_region$date <- as.Date(measure_paca_region$date,format = "%Y-%m-%d") 
 
-#measure_paca <- read.csv(here::here("output","measures",
-#                                    "measure_pa_ca_diagnosis.csv"),
-#                        header=TRUE, sep=",")
+cl_id <- unique(measure_paca_region$region)
 
-#measure_paca <- read_csv(here::here("output","measures",
-#                                    "measure_pa_ca_diagnosis.csv"))
+paca_rate_region <- ggplot(data = measure_paca_region,
+                           aes(date, value)) +
+  geom_line(aes(color = region))+
+  geom_point(aes(color = region))+
+  scale_x_date(date_breaks = "2 month",
+               date_labels = "%Y-%m")+#%B
+  xlab("Time")+
+  ylab("Pa Ca diagnosis by region (rate)")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-#measure_paca$date <- as.Date(measure_paca$date,format = "%Y-%m-%d") 
-#measure_paca <- measure_paca %>% distinct(date, .keep_all = TRUE)
+ggsave(
+  plot= paca_rate_region, dpi=800,width = 20,height = 10, units = "cm",
+  filename="paca_rate_region.png", path=here::here("output"),
+)
 
-#paca_rate <- ggplot(data = measure_paca,
-#                    aes(date, value)) +
-#  geom_line()+
-#  geom_point()+
-#  #stat_summary(fun = sum, geom = "line") + # or "bar"
-#  scale_x_date(date_breaks = "2 month",
-#               date_labels = "%Y-%m")+#%B
-#  xlab("Time")+
-#  ylab("Pa Ca diagn (rate)")+
-#  labs(colour = "Station")+
-#  theme_bw()+
-#  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-#ggsave(
-#  plot= paca_rate, dpi=800,width = 20,height = 10, units = "cm",
-#  filename="paca_rate.png", path=here::here("output"),
-#)
+write.table(measure_paca_region, here::here("output", "paca_rate_region.csv"),
+            col.names= c("region","count", "population", "rate", "date"),sep = ",",row.names = F)
 
 
+########
+#pancreatic cancer rates ***by IMD***
+########
+
+measure_paca_IMD <- read_csv(here::here("output","measures",
+                                           "measure_pa_ca_by_IMD_rate.csv"))
+measure_paca_IMD$date <- as.Date(measure_paca_IMD$date,format = "%Y-%m-%d") 
+
+measure_paca_IMD$imd_cat <- as.character(measure_paca_IMD$imd_cat)
+
+paca_rate_IMD <- ggplot(data = measure_paca_IMD,
+                           aes(date, value)) +
+  geom_line(aes(color = imd_cat))+
+  geom_point(aes(color = imd_cat))+
+  scale_x_date(date_breaks = "2 month",
+               date_labels = "%Y-%m")+#%B
+  xlab("Time")+
+  ylab("Pa Ca diagnosis by IMD (rate)")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggsave(
+  plot= paca_rate_IMD, dpi=800,width = 20,height = 10, units = "cm",
+  filename="paca_rate_IMD.png", path=here::here("output"),
+)
+
+write.table(measure_paca_IMD, here::here("output", "paca_rate_IMD.csv"),
+            col.names= c("imd_cat","count", "population", "rate", "date"),sep = ",",row.names = F)
 
