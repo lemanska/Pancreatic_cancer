001c2820bc68da9bd01fcea9f48e4aa1aee209e4
lemanska <a.lemanska@surrey.ac.uk>
2022-02-20 16:20:13 +0000

    not using measures

---
+++ analysis/analysis1.R
@@ -0,0 +1,23 @@
+
+library('tidyverse')
+
+X <- read_csv(
+  here::here("output", "input.csv"))
+
+X$ca_date <- as.Date(X$ca_date,format = "%Y-%m-%d") 
+X <- X[-which(X$ca_date<"2015-01-01"),]
+
+
+X$Month <- as.Date(cut(X$ca_date, breaks = "month"))
+
+tab1 <- table(X$Month)
+
+paca_time <- ggplot(data = X,
+                    aes(Month, pa_ca)) +
+  stat_summary(fun = sum, geom = "line") 
+
+ggsave(
+  plot= paca_time,
+  filename="paca_time.png", path=here::here("output"),
+)
+
