abda43f34ec3c499dafaa3f10423fde20f96144b
lemanska <a.lemanska@surrey.ac.uk>
2022-03-23 17:43:35 +0000

    commented out unused variables for now

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -44,131 +44,131 @@ study = StudyDefinition(
         },
     ),
 # demographics
-    sex=patients.sex(
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"M": 0.49, "F": 0.51}},
-        }
-    ),
-    ethnicity=patients.categorised_as(
-        {
-            "Missing": "DEFAULT",
-            "White": """ ethnicity_code=1 """,
-            "Mixed": """ ethnicity_code=2 """,
-            "South Asian": """ ethnicity_code=3 """,
-            "Black": """ ethnicity_code=4 """,
-            "Other": """ ethnicity_code=5 """,
-        },
-        return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
-                    "Missing": 0.4,
-                    "White": 0.2,
-                    "Mixed": 0.1,
-                    "South Asian": 0.1,
-                    "Black": 0.1,
-                    "Other": 0.1,
-                }
-            },
-        },
-        ethnicity_code=patients.with_these_clinical_events(
-            ethnicity_codes,
-            returning="category",
-            find_last_match_in_period=True,
-            on_or_before="pa_ca_date",
-            return_expectations={
-            "category": {"ratios": {"1": 0.4, "2": 0.4, "3": 0.2, "4":0.2,"5": 0.2}},
-            "incidence": 0.75,
-            },
-        ),
-    ),
-    msoa=patients.registered_practice_as_of(
-        "pa_ca_date",
-        returning="msoa_code",
-        return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
-                    "E02002488": 0.1,
-                    "E02002586": 0.1,
-                    "E02002677": 0.1,
-                    "E02002814": 0.1,
-                    "E02002915": 0.1,
-                    "E02003251": 0.1,
-                    "E02000003": 0.2,
-                    "E02003334": 0.1,
-                    "E02002986": 0.1,
-                },
-            },
-        },
-    ),
-    stp=patients.registered_practice_as_of(
-        "pa_ca_date",
-        returning="stp_code",
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {
-                "E54000005": 0.25, "E54000006": 0.25,
-                "E54000007": 0.25, "E54000008": 0.25}},
-        },
-    ),
-    imd_Q=patients.address_as_of(
-        "pa_ca_date",
-        returning="index_of_multiple_deprivation",
-        round_to_nearest=100,
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
-        },
-    ),
-    imd_cat=patients.categorised_as(
-        {
-            "0": "DEFAULT",
-            "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
-            "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
-            "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
-            "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
-            "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation < 32844""",
-        },
-        index_of_multiple_deprivation=patients.address_as_of(
-            "pa_ca_date",
-            returning="index_of_multiple_deprivation",
-            round_to_nearest=100,
-        ),
-        return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
-                    "0": 0.05,
-                    "1": 0.19,
-                    "2": 0.19,
-                    "3": 0.19,
-                    "4": 0.19,
-                    "5": 0.19,
-                }
-            },
-        },
-    ),
-    region=patients.registered_practice_as_of(
-        "pa_ca_date",
-        returning="nuts1_region_name",
-        return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
-                    "North East": 0.1,
-                    "North West": 0.1,
-                    "Yorkshire and the Humber": 0.2,
-                    "East Midlands": 0.1,
-                    "West Midlands": 0.1,
-                    "East of England": 0.1,
-                    "London": 0.1,
-                    "South East": 0.2,
-                },
-            },
-        },
-    ),
+    # sex=patients.sex(
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {"ratios": {"M": 0.49, "F": 0.51}},
+    #     }
+    # ),
+    # ethnicity=patients.categorised_as(
+    #     {
+    #         "Missing": "DEFAULT",
+    #         "White": """ ethnicity_code=1 """,
+    #         "Mixed": """ ethnicity_code=2 """,
+    #         "South Asian": """ ethnicity_code=3 """,
+    #         "Black": """ ethnicity_code=4 """,
+    #         "Other": """ ethnicity_code=5 """,
+    #     },
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {
+    #             "ratios": {
+    #                 "Missing": 0.4,
+    #                 "White": 0.2,
+    #                 "Mixed": 0.1,
+    #                 "South Asian": 0.1,
+    #                 "Black": 0.1,
+    #                 "Other": 0.1,
+    #             }
+    #         },
+    #     },
+    #     ethnicity_code=patients.with_these_clinical_events(
+    #         ethnicity_codes,
+    #         returning="category",
+    #         find_last_match_in_period=True,
+    #         on_or_before="pa_ca_date",
+    #         return_expectations={
+    #         "category": {"ratios": {"1": 0.4, "2": 0.4, "3": 0.2, "4":0.2,"5": 0.2}},
+    #         "incidence": 0.75,
+    #         },
+    #     ),
+    # ),
+    # msoa=patients.registered_practice_as_of(
+    #     "pa_ca_date",
+    #     returning="msoa_code",
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {
+    #             "ratios": {
+    #                 "E02002488": 0.1,
+    #                 "E02002586": 0.1,
+    #                 "E02002677": 0.1,
+    #                 "E02002814": 0.1,
+    #                 "E02002915": 0.1,
+    #                 "E02003251": 0.1,
+    #                 "E02000003": 0.2,
+    #                 "E02003334": 0.1,
+    #                 "E02002986": 0.1,
+    #             },
+    #         },
+    #     },
+    # ),
+    # stp=patients.registered_practice_as_of(
+    #     "pa_ca_date",
+    #     returning="stp_code",
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {"ratios": {
+    #             "E54000005": 0.25, "E54000006": 0.25,
+    #             "E54000007": 0.25, "E54000008": 0.25}},
+    #     },
+    # ),
+    # imd_Q=patients.address_as_of(
+    #     "pa_ca_date",
+    #     returning="index_of_multiple_deprivation",
+    #     round_to_nearest=100,
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {"ratios": {"100": 0.1, "200": 0.2, "300": 0.7}},
+    #     },
+    # ),
+    # imd_cat=patients.categorised_as(
+    #     {
+    #         "0": "DEFAULT",
+    #         "1": """index_of_multiple_deprivation >=1 AND index_of_multiple_deprivation < 32844*1/5""",
+    #         "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
+    #         "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
+    #         "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
+    #         "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation < 32844""",
+    #     },
+    #     index_of_multiple_deprivation=patients.address_as_of(
+    #         "pa_ca_date",
+    #         returning="index_of_multiple_deprivation",
+    #         round_to_nearest=100,
+    #     ),
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {
+    #             "ratios": {
+    #                 "0": 0.05,
+    #                 "1": 0.19,
+    #                 "2": 0.19,
+    #                 "3": 0.19,
+    #                 "4": 0.19,
+    #                 "5": 0.19,
+    #             }
+    #         },
+    #     },
+    # ),
+    # region=patients.registered_practice_as_of(
+    #     "pa_ca_date",
+    #     returning="nuts1_region_name",
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {
+    #             "ratios": {
+    #                 "North East": 0.1,
+    #                 "North West": 0.1,
+    #                 "Yorkshire and the Humber": 0.2,
+    #                 "East Midlands": 0.1,
+    #                 "West Midlands": 0.1,
+    #                 "East of England": 0.1,
+    #                 "London": 0.1,
+    #                 "South East": 0.2,
+    #             },
+    #         },
+    #     },
+    # ),
     bmi_before=patients.most_recent_bmi(
         between=["pa_ca_date - 1 years", "pa_ca_date"],
         minimum_age_at_measurement=16,
@@ -394,28 +394,28 @@ study = StudyDefinition(
             "incidence": 0.6,
         },
     ),
-    care_home_type=patients.care_home_status_as_of(
-        "pa_ca_date",
-        categorised_as={
-            "PC":
-            """
-            IsPotentialCareHome
-            AND LocationDoesNotRequireNursing='Y'
-            AND LocationRequiresNursing='N'
-            """,
-            "PN":
-            """
-            IsPotentialCareHome
-            AND LocationDoesNotRequireNursing='N'
-            AND LocationRequiresNursing='Y'
-            """,
-            "PS": "IsPotentialCareHome",
-            "PR": "NOT IsPotentialCareHome",
-            "": "DEFAULT",
-        },
-        return_expectations={
-            "rate": "universal",
-            "category": {"ratios": {"PC": 0.05, "PN": 0.05, "PS": 0.05, "PR": 0.84, "": 0.01},},
-        },
-    ),
+    # care_home_type=patients.care_home_status_as_of(
+    #     "pa_ca_date",
+    #     categorised_as={
+    #         "PC":
+    #         """
+    #         IsPotentialCareHome
+    #         AND LocationDoesNotRequireNursing='Y'
+    #         AND LocationRequiresNursing='N'
+    #         """,
+    #         "PN":
+    #         """
+    #         IsPotentialCareHome
+    #         AND LocationDoesNotRequireNursing='N'
+    #         AND LocationRequiresNursing='Y'
+    #         """,
+    #         "PS": "IsPotentialCareHome",
+    #         "PR": "NOT IsPotentialCareHome",
+    #         "": "DEFAULT",
+    #     },
+    #     return_expectations={
+    #         "rate": "universal",
+    #         "category": {"ratios": {"PC": 0.05, "PN": 0.05, "PS": 0.05, "PR": 0.84, "": 0.01},},
+    #     },
+    # ),
 )
