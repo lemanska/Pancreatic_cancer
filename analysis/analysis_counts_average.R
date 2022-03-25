72f45d07128e9eeb110215a8786d2fd8daf37ffb
lemanska <a.lemanska@surrey.ac.uk>
2022-03-25 15:22:57 +0000

    meds to dmd & stop measures

--- analysis/analysis_counts_average.R
+++ analysis/analysis_counts_average.R
@@ -31,7 +31,7 @@ redactor <- function(n, threshold=6,e_overwrite=NA_integer_){
 #download and prep the data
 ###
 
-Denominator <- read_csv(here::here("output", "measures", "measure_registered_rate.csv"))
+#?# Denominator <- read_csv(here::here("output", "measures", "measure_registered_rate.csv"))
 
 X <- read_csv(here::here("output", "input.csv"))
 X$pa_ca_date <- as.Date(X$pa_ca_date,format = "%Y-%m-%d") 
@@ -55,8 +55,8 @@ monthly_count <- monthly_count %>% mutate_at(vars("pa_ca", "diabetes", "liver_fu
                                                   "emergency_care_after", "gp_consult_count"),
                                              redactor)
 # Merge it with the denominator table to create the monthly registered variable for calcualting rates
-monthly_count <- merge(monthly_count, Denominator[,c("registered", "date")],
-                       by.x = "Month", by.y = "date")
+#?# monthly_count <- merge(monthly_count, Denominator[,c("registered", "date")],
+#?#                        by.x = "Month", by.y = "date")
 
 
 # Summarize monthly stats using median 
@@ -95,30 +95,32 @@ monthly_average[which(is.na(monthly_count$pa_ca)),c("age", "bmi_before", "bmi_af
 # plot monthly rates longitudinal  
 ###
 # calculate rates of pancreatic cancer per 100,000 registered patients 
-monthly_count$rate <- monthly_count$pa_ca / monthly_count$registered * 100000
+#?# monthly_count$rate <- monthly_count$pa_ca / monthly_count$registered * 100000
 # prep axis 
-monthly_count$Year <- as.Date(cut(monthly_count$Month, breaks = "year"))
+#?# monthly_count$Year <- as.Date(cut(monthly_count$Month, breaks = "year"))
 # plot 2015-2022 longitudinal
-paca_time_rates <- ggplot(data = monthly_count,
-                          aes(Month, rate, color = Year)) +
-  geom_line(color = monthly_count$Year )+
-  geom_point(color = monthly_count$Year )+
-  scale_x_date(date_breaks = "2 month",
-               date_labels = "%Y-%m")+
-  labs(title = "Incident pancreatic cancer: rates", 
-       x = "Time", y = "Pancreatic cancer rates per 100,000 patients")+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
-  geom_vline(xintercept =  as.numeric(as.Date("2020-03-26",format = "%Y-%m-%d")), 
-             linetype="solid", color = "blue", size=1)+
-  annotate("text", x = (as.Date("2020-03-26",format = "%Y-%m-%d")+150), 
-           y = max(monthly_count$rate,na.rm = TRUE)-0.1*max(monthly_count$rate,na.rm = TRUE), 
-           label = "lockdown \n start", color = "blue")
+
+#?# #?# #?# #?# #?# #?# #?# #?# 
+#?# paca_time_rates <- ggplot(data = monthly_count,
+#?#                           aes(Month, rate, color = Year)) +
+#?#   geom_line(color = monthly_count$Year )+
+#?#   geom_point(color = monthly_count$Year )+
+#?#   scale_x_date(date_breaks = "2 month",
+#?#                date_labels = "%Y-%m")+
+#?#   labs(title = "Incident pancreatic cancer: rates", 
+#?#        x = "Time", y = "Pancreatic cancer rates per 100,000 patients")+
+#?#   theme_bw()+
+#?#   theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+#?#   geom_vline(xintercept =  as.numeric(as.Date("2020-03-26",format = "%Y-%m-%d")), 
+#?#              linetype="solid", color = "blue", size=1)+
+#?#   annotate("text", x = (as.Date("2020-03-26",format = "%Y-%m-%d")+150), 
+#?#            y = max(monthly_count$rate,na.rm = TRUE)-0.1*max(monthly_count$rate,na.rm = TRUE), 
+#?#            label = "lockdown \n start", color = "blue")
 # save the plot 
-ggsave(
-  plot= paca_time_rates, dpi=800,width = 20,height = 10, units = "cm",
-  filename="paca_time_rates.png", path=here::here("output"),
-)
+#?# ggsave(
+#?#   plot= paca_time_rates, dpi=800,width = 20,height = 10, units = "cm",
+#?#   filename="paca_time_rates.png", path=here::here("output"),
+#?# )
 
 ###
 #plot rates by year
@@ -127,21 +129,21 @@ ggsave(
 monthly_count$MonthNo <- as.numeric(substr(monthly_count$Month, 6, 7))
 monthly_count$YearNo <- substr(monthly_count$Month, 1, 4)
 # plot
-paca_time_rates_year <- ggplot(data = monthly_count,
-                               aes(MonthNo, pa_ca, color = YearNo)) +
-  geom_line()+
-  geom_point()+
-  scale_x_continuous(name = "Time", breaks = c(1:12),
-                     label = format(ISOdatetime(2000,1:12,1,0,0,0),"%b"))+
-  labs(title = "Incident pancreatic cancer: rates", 
-       colour = "Year", x = "Time", y = "Pancreatic cancer rates per 100,000 patients")+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+#?# paca_time_rates_year <- ggplot(data = monthly_count,
+#?#                                aes(MonthNo, pa_ca, color = YearNo)) +
+#?#   geom_line()+
+#?#   geom_point()+
+#?#   scale_x_continuous(name = "Time", breaks = c(1:12),
+#?#                      label = format(ISOdatetime(2000,1:12,1,0,0,0),"%b"))+
+#?#   labs(title = "Incident pancreatic cancer: rates", 
+#?#        colour = "Year", x = "Time", y = "Pancreatic cancer rates per 100,000 patients")+
+#?#   theme_bw()+
+#?#   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 # save
-ggsave(
-  plot= paca_time_rates_year, dpi=800,width = 20,height = 10, units = "cm",
-  filename="paca_time_rates_year.png", path=here::here("output"),
-)
+#?# ggsave(
+#?#   plot= paca_time_rates_year, dpi=800,width = 20,height = 10, units = "cm",
+#?#   filename="paca_time_rates_year.png", path=here::here("output"),
+#?# )
 
 # count longitudinal 
 paca_time_table <- ggplot(data = monthly_count,
